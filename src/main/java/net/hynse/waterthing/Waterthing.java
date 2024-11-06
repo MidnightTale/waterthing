b9c77c9d7c44dc514eef9400fb615dcd176b948d
MidnightTale <midnighttale24@gmail.com>
2024-11-06 13:34:45 +0700

    w

---
+++ src/main/java/net/hynse/waterthing/Waterthing.java
@@ -0,0 +1,17 @@
+package net.hynse.waterthing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Waterthing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
